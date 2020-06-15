347c2fdc56ffd9ee6029d8cada53bc53e45b350d
Renan Muniz Merlo <renan.mmerlo@gmail.com>
2020-06-15 20:58:20 -0300

    Aula 4 Exercicio 19

---
+++ src/metodofinal/Carro.java
@@ -0,0 +1,11 @@
+package metodofinal;
+
+public class Carro {
+    public void ligar(){
+        System.out.println("Carro ligando");
+    }
+
+    public final void correr(){
+        System.out.println("Carro correndo.");
+    }
+}
---
+++ src/metodofinal/Fusca.java
@@ -0,0 +1,14 @@
+package metodofinal;
+
+public class Fusca extends Carro{
+
+    public void ligar(){
+        System.out.println("ligando fusca");
+    }
+
+    //não aceita sobrescrever o metodo final da classe pai Carro
+    //public void correr(){
+    //    System.out.println("fusca correndo");
+    //}
+
+}
