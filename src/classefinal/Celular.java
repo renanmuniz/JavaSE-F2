c0bcbf1a081d83ca043c2d6f7956fdf30f921b98
Renan Muniz Merlo <renan.mmerlo@gmail.com>
2020-06-15 20:20:06 -0300

    Aula 4 Exercicio 18

---
+++ src/classefinal/Celular.java
@@ -0,0 +1,12 @@
+package classefinal;
+
+
+public final class Celular {
+    public void ligar(){
+        System.out.println("fazer uma ligação");
+    }
+
+    public void atender(){
+        System.out.println("Atendendo telefone");
+    }
+}
---
+++ src/classefinal/Lg.java
@@ -0,0 +1,5 @@
+package classefinal;
+
+public class Lg extends Celular{
+    //Não compila porque não é permitido herdar a classe final.
+}
