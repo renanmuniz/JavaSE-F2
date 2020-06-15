3628b949e19cf468bcbba183f0d6110958e8c2fb
Renan Muniz Merlo <renan.mmerlo@gmail.com>
2020-06-14 21:04:25 -0300

    Aula 1 Exercicio3

---
+++ src/comercial/Venda.java
@@ -0,0 +1,21 @@
+package comercial;
+
+public class Venda {
+
+    public String pessoa;
+    public double valor;
+
+    public void vender(){
+        System.out.println(pessoa + "esta vendendo R$" + this.valor);
+    }
+
+    public double calcularImposto(){
+        double dozePct = this.valor * 0.10;
+        return dozePct;
+    }
+
+    public void adicionarJuros(double juros){
+        double novo = this.valor * juros / 100;
+        this.valor += novo;
+    }
+}
