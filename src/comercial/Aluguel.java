cf71a1d79955cd9562fb5444323b2b466670482d
Renan Muniz Merlo <renan.mmerlo@gmail.com>
2020-06-15 18:35:20 -0300

    Aula 2 Exercicio 13

---
+++ src/comercial/Aluguel.java
@@ -0,0 +1,11 @@
+package comercial;
+
+import livraria.Livro;
+
+public class Aluguel {
+    public void Alugar(){
+        Livro l1 = new Livro();
+        // l1.nome = "nome do livro inacessivel, default, só para mesmo pacote.";
+        // só consegueria fazer algo se tivesse metodo get/set public
+    }
+}
---
+++ src/livraria/Locacao.java
@@ -0,0 +1,8 @@
+package livraria;
+
+public class Locacao {
+    public void locar(){
+        Livro l1 = new Livro();
+        l1.nome = "nome do livro acessivel, default, mesmo pacote.";
+    }
+}
